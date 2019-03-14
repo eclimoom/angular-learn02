8dca606977243c854b39c71a8097e63505e18e89
moom <junjihe@live.com>
2019-03-14 10:57:46 +0800

    init

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ]
 })
 export class LoginModule { }
